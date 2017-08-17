efad406215713b2bd2ee154ed28c8e4f217cacdb
Hieu Van <hieuvanphu91@gmail.com>
2017-08-17 13:14:28 +0700

    chore(database): mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const hapi = require('hapi');
+const mongoose = require('mongoose');
 
 const server = new hapi.Server();
 
 server.connection({
   host: '0.0.0.0',
   port: 8080
+});
+
+mongoose.connect('mongodb://192.168.99.100:27017/test_database', {
+  useMongoClient: true
 });
 
 server.start((err) => {
